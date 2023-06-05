26a98b7c82a1d7e7eeba7ca518d0de88e0306c30
Promart1 <marta.batih@gmail.com>
2023-06-06 00:49:50 +0300

    Update ImageGalleryItem.js

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 export class ImageGalleryItem extends Component {
   render() {
-    const { smallUrl, description, bigUrl } = this.props;
+    const { smallUrl, description } = this.props;
     return (
       <li>
         <img src={smallUrl} alt={description} />
